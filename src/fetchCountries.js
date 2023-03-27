47bd93a6170f34a052752a4a3471d9182b8f4ae1
8Yurii8 <114691896+8Yurii8@users.noreply.github.com>
2023-03-27 22:14:56 +0300

    finish 10HW

---
+++ src/fetchCountries.js
@@ -0,0 +1,17 @@
+import Notiflix from 'notiflix';
+
+export function fetchCountries(name) {
+  return fetch(`https://restcountries.com/v3.1/name/${name}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      return data;
+    })
+    .catch(error => {
+      console.log(`Error`);
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,120 @@
+// Імпорт стилі
 import './css/styles.css';
+// Імпорт стилі
+
+// Імпорт ліби
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+// Імпорт ліби
+
+// Імпорт fetch
+import { fetchCountries } from './fetchCountries';
+// Імпорт fetch
 
 const DEBOUNCE_DELAY = 300;
+
+const inputHandleText = document.querySelector('input#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+inputHandleText.addEventListener(
+  'input',
+  debounce(event => {
+    const countriesPromise = fetchCountries(event.target.value.trim());
+    countriesPromise
+      .then(countries => {
+        // перевірка на максимум
+        if (countries.length >= 10) {
+          return Notiflix.Report.info(
+            `Too many matches found. Please enter a more specific name.`
+          );
+        }
+        // перевірка на максимум
+
+        // більше двох
+        const countryListHTML = createCountryList(countries);
+        countryList.innerHTML = countryListHTML;
+        applyCountryListStyles();
+
+        // очищує info
+        if (countries.length > 2) {
+          countryInfo.innerHTML = '';
+          return;
+        }
+        // очищує info
+        // більше двох
+
+        // одна країна
+        if (countries.length < 2) {
+          const countryInfoHTML = createCountryInfo(countries);
+          countryInfo.innerHTML = countryInfoHTML;
+          applyCountryIteamStyles();
+        }
+      })
+      .finally(() => {
+        // Очищує список
+        if (event.target.value === '') {
+          countryList.innerHTML = '';
+          return;
+        }
+        // Очищує список
+      });
+  }, DEBOUNCE_DELAY)
+);
+// додаємо li
+function createCountryList(countries) {
+  return countries
+    .map(({ flags, name }) => {
+      return `<li class="country-list-iteam"><span><img src="${flags.svg}" height="30" width="30" /></span>${name.common}</li>`;
+    })
+    .join('');
+}
+// додаємо li
+
+// додаємо div
+function createCountryInfo(countriesInfo) {
+  return countriesInfo
+    .map(({ capital, population, languages }) => {
+      const languageValues = Object.values(languages).join(', ');
+      return `<p class="courtry__info-text">Capital:<span class="courtry__info-iteam">${capital}</span></p><p class="courtry__info-text">Population:<span class="courtry__info-iteam">${population}</span></p><p class="courtry__info-text">Languages:<span class="courtry__info-iteam">${languageValues}</span></p>`;
+    })
+    .join('');
+}
+// додаємо div
+
+// додаємо стилі до ul
+countryList.style.paddingLeft = '0';
+
+function applyCountryListStyles() {
+  // стилі до li
+  const countryListItems = document.querySelectorAll('.country-list-iteam');
+  countryListItems.forEach(item => {
+    item.style.display = 'flex';
+    item.style.alignItems = 'center';
+    item.style.gap = '20px';
+    item.style.fontWeight = '1000';
+    item.style.fontSize = 'x-large';
+  });
+  // стилі до li
+}
+// додаємо стилі до li
+
+// додаємо стилі до div
+function applyCountryIteamStyles() {
+  // стиль для span
+  const countryInfoIteam = document.querySelectorAll('.courtry__info-iteam');
+  countryInfoIteam.forEach(item => {
+    item.style.marginLeft = '5px';
+    item.style.fontWeight = '400';
+  });
+  // стиль для span
+
+  // стиль для P
+  const countryInfoText = document.querySelectorAll('.courtry__info-text');
+  countryInfoText.forEach(item => {
+    item.style.fontWeight = '1000';
+  });
+  // стиль для P
+}
+// додаємо стилі до div
+
+// по "name.official" на прикладі країни "Sweden" повертає "Kingdom of Sweden" а на відео видно що використовуєтся "name.common" і він якраз повертає "Sweden" якщо так не підходить для здачі ДЗ то я поверно так як описано в ТЗ але тоді нажаль воно не сходится з відео ТЗ
